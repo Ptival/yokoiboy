6629cec54bec2ffb59e81ce6dec8f933b5e7efc9
Valentin Robert <valentin.robert.42@gmail.com>
2024-09-04 16:28:20 -0700

    slightly more precise frame time

--- src/application_state.rs
+++ src/application_state.rs
@@ -1,8 +1,10 @@
 use std::{
     fs::{self, File, OpenOptions},
     io::Write,
-    num::Wrapping,
+    num::{Saturating, Wrapping},
     path::Path,
+    thread::sleep,
+    time::{self, Duration},
 };
 
 use circular_queue::CircularQueue;
@@ -17,6 +19,7 @@ use crate::{
 };
 
 const CPU_SNAPS_CAPACITY: usize = 5;
+const FRAME_TIME_NANOSECONDS: u32 = 16742;
 const LOG_PATH: &str = "log";
 
 #[derive(Debug)]
@@ -25,6 +28,7 @@ pub struct ApplicationState {
     pub output_file: Option<File>,
     pub paused: bool,
     pub snaps: CircularQueue<Machine>,
+    target_frame_time: Duration,
 }
 
 enum PreserveHistory {
@@ -43,6 +47,7 @@ impl ApplicationState {
             .load_rom(&args.game_rom)
             .unwrap_or_else(|e| panic!("Failed to load game ROM: {}", e));
         queue.push(machine);
+        let target_frame_time = Duration::new(0, FRAME_TIME_NANOSECONDS);
         Self {
             breakpoints: breakpoints.into(),
             output_file: if args.log_for_doctor {
@@ -63,6 +68,7 @@ impl ApplicationState {
             },
             paused: false,
             snaps: queue,
+            target_frame_time,
         }
     }
 
@@ -90,7 +96,7 @@ impl ApplicationState {
     }
 
     // TODO: move in machine.rs
-    fn step_machine<'a>(machine: &'a mut Machine) -> &'a mut Machine {
+    fn step_machine<'a>(machine: &'a mut Machine) -> u8 {
         let (mut t_cycles, mut _m_cycles) = Interrupts::handle_interrupts(machine);
         if t_cycles == 0 {
             (t_cycles, _m_cycles) = CPU::execute_one_instruction(machine)
@@ -99,16 +105,17 @@ impl ApplicationState {
         PPU::step_dots(machine, t_cycles);
         machine.t_cycle_count += t_cycles as u64;
 
-        if machine.read_u8(Wrapping(0xFF02)).0 == 0x81 {
-            let char = machine.read_u8(Wrapping(0xFF01));
-            print!("{}", char.0 as char);
-            machine.write_u8(Wrapping(0xFF02), Wrapping(0x01));
-        }
+        // // Print characters written to the Link cable on the terminal (useful for blargg w/o LCD)
+        // if machine.read_u8(Wrapping(0xFF02)).0 == 0x81 {
+        //     let char = machine.read_u8(Wrapping(0xFF01));
+        //     print!("{}", char.0 as char);
+        //     machine.write_u8(Wrapping(0xFF02), Wrapping(0x01));
+        // }
 
-        machine
+        t_cycles
     }
 
-    fn step(&mut self, preserve: PreserveHistory) {
+    fn step(&mut self, preserve: PreserveHistory) -> u8 {
         if !self.current_machine().is_dmg_boot_rom_on()
             && !self.current_machine().cpu().low_power_mode
         {
@@ -121,12 +128,13 @@ impl ApplicationState {
         match preserve {
             PreserveHistory::DontPreserveHistory => {
                 let machine = current_machine;
-                ApplicationState::step_machine(machine);
+                ApplicationState::step_machine(machine)
             }
             PreserveHistory::PreserveHistory => {
                 let mut next_machine = current_machine.clone();
-                ApplicationState::step_machine(&mut next_machine);
+                let t_cycles = ApplicationState::step_machine(&mut next_machine);
                 self.snaps.push(next_machine);
+                t_cycles
             }
         }
     }
@@ -175,18 +183,36 @@ impl ApplicationState {
             Message::ContinueRunUntilBreakpoint => {
                 let mut pc = self.current_machine().registers().pc;
 
-                // Run some number of steps before updating the display
-                let mut remaining_steps: u32 = 10000;
-                while remaining_steps > 0 && !self.paused && !self.breakpoints.contains(&pc.0) {
-                    remaining_steps -= 1;
-                    self.step(PreserveHistory::DontPreserveHistory);
+                let initial_time = time::Instant::now();
+
+                let mut remaining_steps = Saturating(69_905);
+                while remaining_steps.0 > 0 && !self.paused && !self.breakpoints.contains(&pc.0) {
+                    let elapsed_t_cycles = self.step(PreserveHistory::DontPreserveHistory);
+                    remaining_steps -= elapsed_t_cycles as u32;
+                    // self.current_machine().ppu_mut().render();
+                    // let final_frame_time = time::Instant::now() - initial_time;
+                    // if final_frame_time > target_frame_time {
+                    //     println!("Overslept {:?}", final_frame_time - target_frame_time);
+                    // } else {
+                    //     println!("Did not oversleep");
+                    // }
                     pc = self.current_machine().registers().pc;
                 }
 
-                if remaining_steps == 0 {
+                if remaining_steps.0 == 0 {
+                    // If we're stopping for a frame, try to get accurate frame time
                     self.current_machine().ppu_mut().render();
+                    let final_time = time::Instant::now();
+                    let frame_time = final_time - initial_time;
+                    if frame_time.as_nanos() < FRAME_TIME_NANOSECONDS as u128 {
+                        sleep(self.target_frame_time - frame_time);
+                    }
+                    // Note: I think technically we should save this time, so that we can account
+                    // for the application rendering time as part of the next frame time.  Currently
+                    // does not matter much though.
                     Task::done(Message::ContinueRunUntilBreakpoint)
                 } else {
+                    // If we're stopping for a breakpoint, no need for frame accuracy
                     Task::none()
                 }
             }
