f13623edaab3844420224db719d298d7beea7e72
Valentin Robert <valentin.robert.42@gmail.com>
2025-08-04 17:53:17 -0700

    fix handling of tall sprites

--- src/ppu/oam_scan.rs
+++ src/ppu/oam_scan.rs
@@ -32,6 +32,7 @@ pub fn oam_scan(ppu: &mut PPU, obj_fetcher: &mut ObjectFetcher, pixel_fetcher: &
         } else {
             8
         };
+
         for object_offset in (0x00..0x9F).step_by(4) {
             if selected_objects.len() == 10 {
                 break;
@@ -40,14 +41,30 @@ pub fn oam_scan(ppu: &mut PPU, obj_fetcher: &mut ObjectFetcher, pixel_fetcher: &
             let object_min_y_on_screen = (y_screen_plus_16 as u16 as i16) - 16;
             let object_max_y_on_screen = object_min_y_on_screen + object_size - 1;
             if object_min_y_on_screen <= ly as i16 && ly as i16 <= object_max_y_on_screen {
+                let mut tile_index = ppu.object_attribute_memory[object_offset + 2];
+
+                // For tall sprites, we need to adjust the tile index to grab either half
+                if is_bit_set(&ppu.read_lcdc(), LCDC_OBJECT_SIZE_BIT) {
+                    if object_min_y_on_screen <= ly as i16
+                        && ly as i16 <= object_max_y_on_screen - 8
+                    {
+                        // Tile index is LY is intersecting the top half of a tall sprite
+                        tile_index &= 0xFE;
+                    } else {
+                        // Tile index is LY is intersecting the bottom half of a tall sprite
+                        tile_index |= 0x01;
+                    }
+                }
+
                 selected_objects.push_back(Sprite {
                     x_screen_plus_8: ppu.object_attribute_memory[object_offset + 1],
                     y_screen_plus_16,
-                    tile_index: ppu.object_attribute_memory[object_offset + 2],
+                    tile_index,
                     attributes: ppu.object_attribute_memory[object_offset + 3],
                 });
             }
         }
+
         obj_fetcher.selected_objects = selected_objects;
         ppu.switch_to_drawing_pixels(pixel_fetcher);
     }
